a94bbb85ac942d683b70af3caf9f577b6344fed7
bakoe <ba.koe96@gmail.com>
2022-01-18 14:11:40 +0100

    Prototype updating WebGL canvas from react-flow

--- src/visualization/DatacubesVisualization.tsx
+++ src/visualization/DatacubesVisualization.tsx
@@ -1,26 +1,26 @@
 import React, { PropsWithChildren } from 'react';
 
+import { useStore } from 'react-flow-renderer';
+
 import { DatacubesApplication } from './DatacubesApplication';
 
 import classes from '../assets/styles/webgloperate.module.css';
 
-interface DatacubesProps {
-    cellWidthDenominator?: number;
-}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface DatacubesProps {}
 
-export const DatacubesVisualization: React.FC<DatacubesProps> = ({
-    cellWidthDenominator = 64,
-    ...props
-}: PropsWithChildren<DatacubesProps>) => {
+export const DatacubesVisualization: React.FC<DatacubesProps> = ({ ...props }: PropsWithChildren<DatacubesProps>) => {
     const canvasRef = React.useRef<HTMLCanvasElement | null>(null);
     const spinnerRef = React.useRef<HTMLDivElement | null>(null);
     const [application, setApplication] = React.useState<DatacubesApplication | undefined>(undefined);
 
+    const transform = useStore((store) => store.transform);
+
     React.useEffect(() => {
         if (application) {
-            (application as DatacubesApplication).cellWidth = 1.0 / cellWidthDenominator;
+            (application as DatacubesApplication).cellWidth = 1.0 / transform[0];
         }
-    }, [application, cellWidthDenominator]);
+    }, [application, JSON.stringify(transform)]);
 
     React.useEffect(() => {
         if (canvasRef.current) {
@@ -28,7 +28,7 @@ export const DatacubesVisualization: React.FC<DatacubesProps> = ({
 
             exampleInstance.initialize(canvasRef.current, spinnerRef.current || undefined);
 
-            (exampleInstance as DatacubesApplication).cellWidth = 1.0 / cellWidthDenominator;
+            (exampleInstance as DatacubesApplication).cellWidth = 1.0 / 64.0;
 
             setApplication(exampleInstance);
         }
