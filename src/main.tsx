98a78a6f9eb6a479ed702d018a34cc357a7d5c71
bakoe <ba.koe96@gmail.com>
2022-02-01 17:06:25 +0100

    Add fix for Safari not using CSS cursors on drag

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// Fix Safari otherwise always using a "text" cursor while dragging on a page
+// https://stackoverflow.com/questions/47295211/safari-wrong-cursor-when-dragging
+document.onselectstart = function () {
+    return false;
+};
+
 // Fall back to ArrayBuffer if the does not support SharedArrayBuffer
 // (or if the website does not fulfil the cross-origin isolation criteria)
 if (self.SharedArrayBuffer !== undefined) {
