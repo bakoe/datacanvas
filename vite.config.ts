249fe451d9b2e08dff3b7186357ac9e9fca694d5
bakoe <ba.koe96@gmail.com>
2022-02-08 19:56:21 +0100

    Remove Vite base (due to Vercel deployment)

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import glsl from 'vite-plugin-glsl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/haeley-datacubes/',
     plugins: [react(), glsl()],
     server: {
         headers: {
