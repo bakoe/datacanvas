27532c17a0fab179b472815f74d1c313f517468c
bakoe <ba.koe96@gmail.com>
2022-02-14 14:23:36 +0100

    Add type, preset, # stops select to Color Mapping

--- src/data/nodes/ColorMappingNode.tsx
+++ src/data/nodes/ColorMappingNode.tsx
@@ -1,4 +1,4 @@
-import { FC, memo, useEffect } from 'react';
+import { FC, memo, useEffect, useState } from 'react';
 import { Connection, Edge, Handle, Node, Position } from 'react-flow-renderer/nocss';
 
 import { Column as CSVColumn } from '@lukaswagner/csv-parser';
@@ -31,11 +31,17 @@ export const ColorMappingNodeSourceHandlesDatatypes: Map<ColorMappingNodeSourceH
 
 export interface ColorMappingNodeState {
     isPending: boolean;
+    colorScaleConfig?: ColorScaleConfig;
+    numberOfStops?: number;
     colorPalette?: ColorPalette;
     column?: CSVColumn;
 }
 
-export const defaultState = { isPending: true } as ColorMappingNodeState;
+export const defaultState = {
+    isPending: true,
+    colorScaleConfig: { type: 'sequential', identifier: 'YlGnBu' },
+    numberOfStops: 5,
+} as ColorMappingNodeState;
 
 export interface ColorMappingNodeData {
     onChangeState: (state: Partial<ColorMappingNodeState>) => void;
@@ -47,12 +53,47 @@ type ColorMappingNodeProps = NodeWithStateProps<ColorMappingNodeData>;
 
 const onConnect = (params: Connection | Edge) => console.log('handle onConnect on ColorMappingNode', params);
 
+interface ColorScaleConfig {
+    type: string;
+    identifier: string;
+}
+
 const ColorMappingNode: FC<ColorMappingNodeProps> = ({ isConnectable, selected, data }) => {
     const { state, onChangeState, isValidConnection } = data;
-    const { isPending = true, colorPalette = undefined as undefined | ColorPalette, column = undefined } = { ...defaultState, ...state };
+    const {
+        isPending = defaultState.isPending,
+        colorPalette = undefined as undefined | ColorPalette,
+        column = undefined,
+        colorScaleConfig = defaultState.colorScaleConfig,
+        numberOfStops = defaultState.numberOfStops,
+    } = { ...defaultState, ...state };
+    const [availableColorScales, setAvailableColorScales] = useState([] as ColorScaleConfig[]);
 
     useEffect(() => {
-        ColorScale.fromPreset('/colorbrewer.json', 'YlGnBu', 5).then((colorScale) => {
+        const loadColorScales = async () => {
+            const response = await fetch('/colorbrewer.json');
+            const colorbrewerJSON = (await response.json()) as Array<{
+                identifier: string;
+                type: string;
+                format: string;
+                colors: Array<Array<number>>;
+            }>;
+            const colorScales = [] as ColorScaleConfig[];
+            for (let presetIndex = 0; presetIndex < colorbrewerJSON.length; presetIndex++) {
+                const preset = colorbrewerJSON[presetIndex];
+                colorScales[presetIndex] = {
+                    identifier: preset.identifier,
+                    type: preset.type,
+                };
+            }
+            setAvailableColorScales(colorScales);
+        };
+
+        loadColorScales();
+    }, []);
+
+    useEffect(() => {
+        ColorScale.fromPreset('/colorbrewer.json', colorScaleConfig.identifier, numberOfStops).then((colorScale) => {
             const colorPalette = colorScale.colors.map((color, index) => {
                 const rgbaUint8 = color.rgbaUI8;
                 const r = rgbaUint8[0];
@@ -65,11 +106,16 @@ const ColorMappingNode: FC<ColorMappingNodeProps> = ({ isConnectable, selected, 
                 };
             });
 
+            // TODO: Add intermediate steps to simulate non-interpolated color scale with hard stops
+            // if (colorScaleConfig.interpolate === false) {
+            // ...
+            // }
+
             onChangeState({
                 colorPalette,
             });
         });
-    }, []);
+    }, [numberOfStops, JSON.stringify(colorScaleConfig)]);
 
     useEffect(() => {
         if (column && colorPalette) {
@@ -78,6 +124,29 @@ const ColorMappingNode: FC<ColorMappingNodeProps> = ({ isConnectable, selected, 
             });
         }
     }, [column, colorPalette]);
+
+    const availableColorScaleTypes = availableColorScales.reduce(
+        (types, colorScale) => (!types.includes(colorScale.type) ? types.concat(colorScale.type) : types),
+        [] as string[],
+    );
+
+    const availableColorScalePresets = availableColorScales.filter((colorScale) => colorScale.type === colorScaleConfig.type);
+
+    useEffect(() => {
+        if (availableColorScalePresets.length > 0) {
+            if (
+                availableColorScalePresets.findIndex(
+                    (colorScale) => colorScale.identifier === colorScaleConfig.identifier && colorScale.type === colorScaleConfig.type,
+                ) !== -1
+            ) {
+                return;
+            }
+            // Select the first (default) color scale if the type changed, i.e., if the previously selected preset does not fit the now selected type
+            onChangeState({
+                colorScaleConfig: availableColorScalePresets[0],
+            });
+        }
+    }, [JSON.stringify(availableColorScalePresets)]);
 
     return (
         <div className={`react-flow__node-default node ${selected && 'selected'} ${isPending && 'pending'}`}>
@@ -103,6 +172,82 @@ const ColorMappingNode: FC<ColorMappingNodeProps> = ({ isConnectable, selected, 
             <hr className="divider" />
 
             <div className="nodrag">
+                <table style={{ textAlign: 'right', width: 'calc(100% + 2px)', borderSpacing: '2px' }}>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <label htmlFor="type">Type:</label>
+                            </td>
+                            <td>
+                                <select
+                                    id="type"
+                                    value={colorScaleConfig.type}
+                                    onChange={(event) => {
+                                        onChangeState({
+                                            colorScaleConfig: {
+                                                ...colorScaleConfig,
+                                                type: event.target.value,
+                                            },
+                                        });
+                                    }}
+                                >
+                                    {availableColorScaleTypes.map((colorScaleType) => (
+                                        <option key={colorScaleType} value={colorScaleType}>
+                                            {colorScaleType}
+                                        </option>
+                                    ))}
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label htmlFor="preset">Preset:</label>
+                            </td>
+                            <td>
+                                <select
+                                    id="preset"
+                                    value={colorScaleConfig.identifier}
+                                    onChange={(event) => {
+                                        onChangeState({
+                                            colorScaleConfig: {
+                                                ...colorScaleConfig,
+                                                identifier: event.target.value,
+                                            },
+                                        });
+                                    }}
+                                >
+                                    {availableColorScalePresets.map((colorScale) => (
+                                        <option key={`${colorScale.type}-${colorScale.identifier}`} value={colorScale.identifier}>
+                                            {colorScale.identifier}
+                                        </option>
+                                    ))}
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label htmlFor="number-of-stops"># stops:</label>
+                            </td>
+                            <td>
+                                <input
+                                    value={numberOfStops}
+                                    onChange={(event) => {
+                                        onChangeState({
+                                            numberOfStops: event.target.valueAsNumber,
+                                        });
+                                    }}
+                                    type="number"
+                                    min="1"
+                                    max="32"
+                                    id="number-of-stops"
+                                />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div className="nodrag" style={{ marginTop: '0.4rem' }}>
                 <EditableColorGradient
                     palette={colorPalette || []}
                     onChangePalette={(palette) => {
